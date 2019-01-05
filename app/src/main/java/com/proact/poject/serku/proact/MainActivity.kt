56bd7f9e03a03110e204e328bfb04ff5462764b0
SKushnaryov <ser.kushnaryov@gmail.com>
2019-01-05 19:52:24 +0300

    initial commit

---
+++ app/src/main/java/com/proact/poject/serku/proact/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.proact.poject.serku.proact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
