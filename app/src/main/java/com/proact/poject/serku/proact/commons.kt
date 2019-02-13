cf206097a5e859b16b40ee13eb8c4a866954b021
SKushnaryov <ser.kushnaryov@gmail.com>
2019-02-13 21:46:46 +0300

    LogScreen logic added

--- app/src/main/java/com/proact/poject/serku/proact/commons.kt
+++ app/src/main/java/com/proact/poject/serku/proact/commons.kt
@@ -1,3 +1,22 @@
 package com.proact.poject.serku.proact
 
+import android.content.Context
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+
 typealias AnyMap = Map<String, Any>
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun TextInputLayout.isTextEmpty(errorMessage: String): Boolean {
+    if (editText?.text.toString().isEmpty()) {
+        error = errorMessage
+        return true
+    } else {
+        error = null
+    }
+
+    return false
+}
