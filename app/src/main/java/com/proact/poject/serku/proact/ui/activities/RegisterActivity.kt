ca9a51ef666e5165f21133f43613fc498fca3e43
SKushnaryov <ser.kushnaryov@gmail.com>
2019-02-20 22:48:52 +0300

    navigation destinations added

--- app/src/main/java/com/proact/poject/serku/proact/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/proact/poject/serku/proact/ui/activities/RegisterActivity.kt
@@ -2,9 +2,15 @@ package com.proact.poject.serku.proact.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.proact.poject.serku.proact.R
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
 }
