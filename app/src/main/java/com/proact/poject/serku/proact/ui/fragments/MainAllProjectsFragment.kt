a2263991f1a29b67e749049d80c783aaca5b3245
SKushnaryov <ser.kushnaryov@gmail.com>
2019-03-03 14:41:47 +0300

    AllProjects fragment updated with loading

--- app/src/main/java/com/proact/poject/serku/proact/ui/fragments/MainAllProjectsFragment.kt
+++ app/src/main/java/com/proact/poject/serku/proact/ui/fragments/MainAllProjectsFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.proact.poject.serku.proact.R
 import com.proact.poject.serku.proact.ui.adapters.ProjectsAdapter
 import com.proact.poject.serku.proact.viewmodels.ProjectViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_projects.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -22,6 +24,7 @@ class MainAllProjectsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val layout = inflater.inflate(R.layout.fragment_projects, container, false)
+        val progressBar = activity?.progressBar
 
         val rv = layout.allProjectsRv
         val adapter = ProjectsAdapter()
@@ -29,8 +32,27 @@ class MainAllProjectsFragment : Fragment() {
         rv.adapter = adapter
         rv.layoutManager = LinearLayoutManager(context)
 
+        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    projectViewModel.getNextProjects(1)
+                }
+            }
+        })
+
         projectViewModel.projects.observe(this, Observer {
             adapter.submitList(it)
+            adapter.notifyDataSetChanged()
+        })
+
+        projectViewModel.loadingStatus.observe(this, Observer {
+            if (it) {
+                progressBar?.visibility = View.VISIBLE
+            } else {
+                progressBar?.visibility = View.INVISIBLE
+            }
         })
 
         projectViewModel.getProjectsByStatus(1)
