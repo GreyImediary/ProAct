aea5b396fda10c59662f0fc1a365dbbcd6336754
SKushnaryov <ser.kushnaryov@gmail.com>
2019-03-08 18:39:25 +0300

    DetailedProjectActivity added

---
+++ app/src/main/java/com/proact/poject/serku/proact/ui/activities/DetailedProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.proact.poject.serku.proact.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.proact.poject.serku.proact.R
+
+class DetailedProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_project)
+    }
+}
