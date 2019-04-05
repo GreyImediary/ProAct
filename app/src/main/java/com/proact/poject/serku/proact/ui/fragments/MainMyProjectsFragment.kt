a8a984560990898fab20bdb839b366ce88376956
SKushnaryov <ser.kushnaryov@gmail.com>
2019-04-05 23:39:53 +0300

    MainMyProjectsFragment realized

--- app/src/main/java/com/proact/poject/serku/proact/ui/fragments/MainMyProjectsFragment.kt
+++ app/src/main/java/com/proact/poject/serku/proact/ui/fragments/MainMyProjectsFragment.kt
@@ -7,20 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.proact.poject.serku.proact.CURRENT_USER_ID_PREF
-import com.proact.poject.serku.proact.R
-import com.proact.poject.serku.proact.SHARED_PREF_NAME
-import com.proact.poject.serku.proact.data.Project
-import com.proact.poject.serku.proact.onSelected
+import androidx.recyclerview.widget.RecyclerView
+import com.proact.poject.serku.proact.*
 import com.proact.poject.serku.proact.ui.adapters.ProjectsAdapter
+import com.proact.poject.serku.proact.ui.adapters.WorkerRequestsAdapter
 import com.proact.poject.serku.proact.viewmodels.ProjectViewModel
+import com.proact.poject.serku.proact.viewmodels.RequestViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_my_projects.*
 import kotlinx.android.synthetic.main.fragment_my_projects.view.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class MainMyProjectsFragment : Fragment() {
-
     private val projectViewModel: ProjectViewModel by sharedViewModel()
-    private val adapter = ProjectsAdapter()
+    private val requestViewModel: RequestViewModel by sharedViewModel()
+    private val curatorActivewProjectsAdapter = ProjectsAdapter()
+    private val curatorFinishedProjectsAdapter = ProjectsAdapter()
+    private val curatorRequestsProjectAdapter = ProjectsAdapter()
+    private val requestAdapter = WorkerRequestsAdapter()
+    private val workerActiveProjects = ProjectsAdapter()
+    private val workerFinishedProjects = ProjectsAdapter()
+    private var onScrollListener: RecyclerView.OnScrollListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,37 +36,154 @@ class MainMyProjectsFragment : Fragment() {
     ): View? {
         val layout = inflater.inflate(R.layout.fragment_my_projects, container, false)
 
-        val projects = HashMap<String, List<Project>>()
 
         val prefs = activity?.getSharedPreferences(SHARED_PREF_NAME, 0)
-        val userId = prefs?.getInt(CURRENT_USER_ID_PREF, -1)
-
-        projectViewModel.getUserProjects(userId!!)
+        val userId = prefs?.getInt(CURRENT_USER_ID_PREF, -1) ?: -1
+        val userGroup = prefs?.getInt(CURRENT_USER_USER_GROUP_PREF, -1) ?: -1
 
         layout.myProjectsRv.also {
             it.layoutManager = LinearLayoutManager(context)
-            it.adapter = adapter
         }
 
 
+
         layout.myProjectsTabs.onSelected(
-            onReselect = { submitProjectData(it?.position, projects) },
-            onSelect = { submitProjectData(it?.position, projects) }
+            onReselect = { getDataByUserGroupAndTab(userGroup, it?.position, userId) },
+            onSelect = { getDataByUserGroupAndTab(userGroup, it?.position, userId) }
         )
 
-        projectViewModel.userProjects.observe(this, Observer {
-            projects.putAll(it)
-            layout.myProjectsTabs.getTabAt(0)?.select()
+        requestViewModel.workerRequests.observe(this, Observer {
+            requestAdapter.submitList(it)
+            requestAdapter.notifyDataSetChanged()
+        })
+
+        projectViewModel.curatorActiveProjects.observe(this, Observer {
+            curatorActivewProjectsAdapter.submitList(it)
+            curatorActivewProjectsAdapter.notifyDataSetChanged()
+        })
+
+        projectViewModel.curatorFinishedProjects.observe(this, Observer {
+            curatorFinishedProjectsAdapter.submitList(it)
+            curatorFinishedProjectsAdapter.notifyDataSetChanged()
+        })
+
+        projectViewModel.curatoRequestProjects.observe(this, Observer {
+            curatorRequestsProjectAdapter.submitList(it)
+            curatorRequestsProjectAdapter.notifyDataSetChanged()
+        })
+
+        projectViewModel.activeUserProjects.observe(this, Observer {
+            workerActiveProjects.submitList(it)
+        })
+
+        projectViewModel.finishedUserProjects.observe(this, Observer {
+            workerFinishedProjects.submitList(it)
+        })
+
+        projectViewModel.loadingStatus.observe(this, Observer {
+            if (it) {
+                progressBar?.visibility = View.VISIBLE
+            } else {
+                progressBar?.visibility = View.INVISIBLE
+            }
         })
 
         return layout
     }
 
-    private fun submitProjectData(tabPosition: Int?, projectData: Map<String, List<Project>>) {
-        if (tabPosition == 0) {
-            adapter.submitList(projectData["active"])
-        } else {
-            adapter.submitList(projectData["finished"])
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        myProjectsTabs.getTabAt(0)?.select()
+    }
+
+    private fun getDataByUserGroupAndTab(userGroup: Int, tabPosition: Int?, userId: Int) {
+
+        if (userGroup == 1) {
+
+            when(tabPosition) {
+                0 -> {
+                    myProjectsRv.adapter = workerActiveProjects
+                    projectViewModel.getActiveUserProjects(userId)
+                }
+                1 -> {
+                    myProjectsRv.adapter = workerFinishedProjects
+                    projectViewModel.getFinishedUserProjects(userId)
+                }
+                2 -> {
+                    requestViewModel.getWorkerRequests(userId)
+                    setNextWokerRequestsListener(userId)
+                }
+            }
+
+        } else if (userGroup == 2) {
+
+
+            when (tabPosition) {
+                0 -> {
+                    projectViewModel.getCuratorActiveProjects(userId)
+                    setNextCuratorsProjectsListener(userId, 0)
+                }
+                1 -> {
+                    projectViewModel.getCuratorFinishedProjects(userId)
+                    setNextCuratorsProjectsListener(userId, 1)
+                }
+                2 -> {
+                    projectViewModel.getCuratorRequestProjects(userId)
+                    setNextCuratorsProjectsListener(userId, 2)
+                }
+            }
         }
+    }
+
+    private fun setNextWokerRequestsListener(workerId: Int) {
+
+        myProjectsRv.adapter = requestAdapter
+
+        if (onScrollListener != null) {
+            myProjectsRv.removeOnScrollListener(onScrollListener!!)
+        }
+
+        onScrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    requestViewModel.getNextRequests(workerId)
+                }
+            }
+        }
+
+        myProjectsRv.addOnScrollListener(onScrollListener!!)
+    }
+
+    private fun setNextCuratorsProjectsListener(curatorId: Int, tabPosition: Int) {
+
+        when (tabPosition) {
+            0 -> myProjectsRv.adapter = curatorActivewProjectsAdapter
+            1 -> myProjectsRv.adapter = curatorFinishedProjectsAdapter
+            2 -> myProjectsRv.adapter = curatorRequestsProjectAdapter
+        }
+
+        if (onScrollListener != null) {
+            myProjectsRv.removeOnScrollListener(onScrollListener!!)
+        }
+
+        onScrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    when (tabPosition) {
+                        0 -> projectViewModel.getNextActiveProjects(curatorId)
+                        1 -> projectViewModel.getNextFinishedProjects(curatorId)
+                        2 -> projectViewModel.getNextRequestProjects(curatorId)
+                    }
+                }
+            }
+        }
+
+        myProjectsRv.addOnScrollListener(onScrollListener!!)
+
     }
 }
